164e47b2f8f4f816fbea4cc7e276024a17587306
Cleberson Osorio <cleberson.osorio@hotmail.com>
2020-06-22 19:50:05 -0300

    desafio 1-2 finalizado

---
+++ 1-intro-programacao-a-programacao-web/desafio-1-2/desafio-1-2.js
@@ -0,0 +1,16 @@
+const nome = 'Edson'
+const sexo = 'F'
+const idade = 48
+const contribuicao = 37
+
+const tempContri = 48 + 38
+
+if (tempContri > 85) {
+    if( contribuicao >= 30 && sexo == 'F') {
+        console.log(`${nome}, voce pode se aposentar!`)
+    } else {
+        console.log(`${nome}, voce pode se aposentar!`)
+    }
+} else {
+    console.log(`${nome}, voce ainda não pode se aposentar!`)
+}
