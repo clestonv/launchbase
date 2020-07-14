151a24f9f824227aca55b5b0af121555d773a297
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-07-14 15:07:20 -0300

    config nunjucks

--- 3-iniciando-no-back-end/server.js
+++ 3-iniciando-no-back-end/server.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.get("/", function(req, res) { //criando rotas
-    return res.send("Hi!!! How's gooing")
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views",{
+    express:server
 })
+
+server.get("/", function(req, res) { //criando rotas
+    return res.render("about")
+})
+
+server.get("/portifolio", function(req, res) { //criando rotas
+    return res.render("portifolio")
+})
+
 server.listen(5000, function() {
     console.log('Server is running')
 })
