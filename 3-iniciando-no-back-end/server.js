d49ab3872e8d0ea05a934755d9ad6cfdb1c40742
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-07-14 17:28:39 -0300

    module dates dinamics finish

--- 3-iniciando-no-back-end/server.js
+++ 3-iniciando-no-back-end/server.js
@@ -2,21 +2,34 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views",{
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) { //criando rotas
-    return res.render("about")
+    const about = {
+        avatar_url: "https://media-exp1.licdn.com/dms/image/C4D03AQFNm0oqdJd6gg/profile-displayphoto-shrink_200_200/0?e=1600300800&v=beta&t=5PVtzkhwgfrU_-xF5KLIigaA9lPRwiqa6j6ARQRcc1o",
+        name: "Cleberson Osorio",
+        role: "QA - Senff",
+        description: 'Programamador iniciante com foco em Full Stack. Colaborador na <a href="https://www.senff.com.br/" target="_blank">Senff</a>',
+        links: [
+            { name: "GitHub", url: "https://github.com/clestonv"},
+            { name: "Instagram", url: "https://www.instagram.com/cleberson_osorio/"},
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/cleberson-osoriov/"}
+        ]
+    }
+    return res.render("about", { about: about })
 })
 
 server.get("/portifolio", function(req, res) { //criando rotas
-    return res.render("portifolio")
+    return res.render("portifolio", { items: videos })
 })
 
 server.listen(5000, function() {
