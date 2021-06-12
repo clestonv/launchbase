05712e938a66d89bd57bb25c54adf3d1752d0c84
Cleberson Osorio <cleberson.osorio@maisinteligencia.com.br>
2021-06-12 13:57:16 -0300

    Update files

---
+++ 5-trabalhando-com-banco-de-dados/src/lib/utils.js
@@ -0,0 +1,36 @@
+module.exports = {
+    age(timestamp) {
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if (month < 0 || month == 0 &&
+            today.getDate() < birthDate.getDate()) {
+            age = age - 1
+        }
+
+        return age
+    },
+    date(timestamp){
+        // Criando um novo objeto de data com o valor que vem no timestamp
+        const date = new Date(timestamp)
+
+        // YYYY ano
+        const year = date.getUTCFullYear()
+        // MM sabendo que o 0 ZERO e 11 ONZE é Dezembro coloco +1
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        // DD 
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return {
+            day,
+            month,
+            year,
+            iso: `${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`,
+            format: `${day}/${month}/${year}`
+        }        
+    }
+}
